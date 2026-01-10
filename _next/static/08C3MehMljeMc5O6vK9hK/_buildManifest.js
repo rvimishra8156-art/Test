2e37c4069b49a8b574b29fc5a61da7e26724c0d7
rvimishra8156-art <rvimishra8156-art@users.noreply.github.com>
2026-01-10 13:23:19 +0000

    deploy: ef0d196975d09fd9192657952e95dbd7b8adc21c

--- _next/static/08C3MehMljeMc5O6vK9hK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-e34b0613cf98ea36.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],"/contact":["static/chunks/pages/contact-528363aa6bbc4f93.js"],sortedPages:["/","/_app","/_error","/contact"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
